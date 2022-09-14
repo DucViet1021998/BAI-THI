c797dee13d25838969db2739b30d6525a8e28ed2
DucViet1021998 <viethg1021998@gmail.com>
2022-09-14 12:06:27 +0700

    update

--- bai2/jav.js
+++ bai2/jav.js
@@ -2,9 +2,11 @@ function numberOneTriangle(a) {
   let b = [];
 for (let i = 1; i<=a ; i++){
   for( let j = 1; j<=i; j++){
+    document.write('*')
   }
    b.push('*');
    console.log(b.join(''));
+   document.write('<br>')
 }
 }
 numberOneTriangle(10);
